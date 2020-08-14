41310ffaf10955417c210110564ade08432418a8
frau0006 <frau0006@algonquinlive.com>
2020-08-14 03:10:48 -0400

    Inserted the default SFML test code, everything functionning normally.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include "SFML/Graphics.hpp"
-#include <stdio.h>
+#include <iostream>
 
 int main() {
+	std::cout << "Hello, Console!" << std::endl;
+
+	sf::RenderWindow window(sf::VideoMode(640, 480), "Empty Window");
+
+	while (window.isOpen()) {
+		
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		
+		window.display();
+	}
+
 	return EXIT_SUCCESS;
 }
